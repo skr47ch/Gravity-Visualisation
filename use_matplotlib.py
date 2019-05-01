be42e971ba04f393fa41c7bc12329b19180ffe78
Shashank <shashank.kanaujia.1@gmail.com>
2019-05-01 23:35:39 +1000

    points are now warped in one direction.

--- use_matplotlib.py
+++ use_matplotlib.py
@@ -4,26 +4,36 @@ import matplotlib.pyplot as plt
 from math import *
 
 # TODO - Interpolation
-plt.axis([-10, 10, -10, 10])
-origin = [0, 0]
-# points = np.array([[x, y] for x in range(1, 6) for y in range(-20, 20, 1)], dtype='f')
 
-A = []
-for i in range(5):
-    A.append([[x, y] for x in range(i, i+1) for y in range(-9, 9)])
+plt.axis([-10, 10, 0, 10])
+mass = [0, 4]
+points = []
 
-points = np.array(A)
 
-for i in range(len(points)):
-    plt.plot(points[i, :, 0], points[i, :, 1])
 
-# for i in range(len(points)):
-#     for index, value in enumerate(points[i]):
-#         d = 1/sqrt(value[i, 0]**2 + value[i, 1]**2)
-#         points[i, index] = [value[i, 0]-d**2, value[i, 1]]
-#         print(d, points[i, index])
+for i in range(-10, 10):
+    points.append([[x, y] for x in range(i, i+1) for y in range(0, 11)])
+
+points = np.array(points, dtype='f')
 
 for i in range(len(points)):
-    plt.plot(points[i, :, 0], points[i, :, 1])
+    plt.plot(points[i, :, 0], points[i, :, 1], color='pink')
 
+
+for index_, value_ in enumerate(points):
+    for index, value in enumerate(points[index_]):
+        try:
+            d = 1/sqrt((mass[0] - value[0]) ** 2 + (mass[1] - value[1]) ** 2)
+        except ZeroDivisionError:
+            d = 0
+            # print('Divide by zero? ダメだ!!')
+        if mass[0] < value[0]:
+            points[index_, index] = value[0]-d**2, value[1]
+        elif mass[0] > value[0]:
+            points[index_, index] = value[0]+d**2, value[1]
+
+for i in range(len(points)):
+    plt.plot(points[i, :, 0], points[i, :, 1], color='green')
+
+plt.plot(mass[0], mass[1], 'o')
 plt.show()
