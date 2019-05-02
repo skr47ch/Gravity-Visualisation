88c67a5e3057fd6168e3491086a67d853ac709b8
Shashank <shashank.kanaujia.1@gmail.com>
2019-05-02 23:22:15 +1000

    minor changes.

--- use_matplotlib.py
+++ use_matplotlib.py
@@ -2,11 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import *
+from scipy.interpolate import interp1d
+import scipy as sp
 
 # TODO - Interpolation
 
+MASS_ = .7
 plt.axis([-10, 10, 0, 10])
-mass = [0, 4]
+object_ = [0, 4]
 points = []
 
 
@@ -23,17 +26,18 @@ for i in range(len(points)):
 for index_, value_ in enumerate(points):
     for index, value in enumerate(points[index_]):
         try:
-            d = 1/sqrt((mass[0] - value[0]) ** 2 + (mass[1] - value[1]) ** 2)
+            d = MASS_ * 1 / sqrt((object_[0] - value[0]) ** 2 + (object_[1] - value[1]) ** 2)
         except ZeroDivisionError:
             d = 0
             # print('Divide by zero? ダメだ!!')
-        if mass[0] < value[0]:
+        if object_[0] < value[0]:
             points[index_, index] = value[0]-d**2, value[1]
-        elif mass[0] > value[0]:
+        elif object_[0] > value[0]:
             points[index_, index] = value[0]+d**2, value[1]
 
 for i in range(len(points)):
     plt.plot(points[i, :, 0], points[i, :, 1], color='green')
 
-plt.plot(mass[0], mass[1], 'o')
+plt.plot(object_[0], object_[1], 'o')
+
 plt.show()
